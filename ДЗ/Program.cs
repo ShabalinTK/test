6dda3ef91a1ba058f5a38ab56490d21657ec1d30
ShabalinTK <108810276+ShabalinTK@users.noreply.github.com>
2023-10-03 18:21:33 +0300

    Update Program.cs

--- ДЗ/Program.cs
+++ ДЗ/Program.cs
@@ -28,35 +28,35 @@ class Prog
 //ЗАДАНИЕ 2-3
 
 //using System;
-//class Prog
-//{
-//    static void Main(string[] args)
-//    {
-//        int number = int.MinValue;
-//        int count = 0;
-//        int number1 = 0; int number2 = 0;
-//        int[] array = {8, 4, 5, 8, 2, 5, 7, 8};
-//        for (int i = 0; i < array.Length; i++)
-//        {
-//            if (number < array[i])
-//            {
-//                number = array[i];
-//                number1 = i;
-//            }
-//        }
-//        for (int i = 0; i < array.Length;  i++)
-//        {
-//            if (number == array[i])
-//            {
-//                count++;
-//                number2 = i;
-//            }
-//        }
-//        Console.WriteLine(number1);
-//        Console.WriteLine(number2);
-//        Console.WriteLine(number);
-//        Console.WriteLine(count);
-//    }
-//}
+class Prog
+{
+    static void Main(string[] args)
+    {
+        int number = int.MinValue;
+        int count = 0;
+        int number1 = 0; int number2 = 0;
+        int[] array = {8, 4, 5, 8, 2, 5, 7, 8};
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (number < array[i])
+            {
+                number = array[i];
+                number1 = i;
+            }
+        }
+        for (int i = 0; i < array.Length;  i++)
+        {
+            if (number == array[i])
+            {
+                count++;
+                number2 = i;
+            }
+        }
+        Console.WriteLine(number1);
+        Console.WriteLine(number2);
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+    }
+}
 
 
