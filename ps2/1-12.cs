e6b4d57139a4223594475b39e921e542db1918f2
ShabalinTK <108810276+ShabalinTK@users.noreply.github.com>
2023-10-17 22:53:04 +0300

    Add files via upload

---
+++ ps2/1-12.cs
@@ -0,0 +1,30 @@
+﻿//1-12 задача
+
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(Math.Cos(x), 2));
+Console.WriteLine(Cos2(x));
+
+static double Cos2(double x, double eps = 0.0000001)
+{
+    double res = 1;
+    double k = 1;
+    double check = Math.Pow(Math.Cos(x), 2);
+    double doublefactorial = 2;
+    double n = 2;
+    double minusone = -1;
+    double pow2 = 2;
+    double powx = x * x;
+    while (Math.Abs(check - res) > eps)
+    {
+        res += minusone * pow2 * powx / doublefactorial;
+        k++;
+        minusone *= -1;
+        pow2 *= 4;
+        powx *= x * x;
+        n += 2;
+        doublefactorial = n * (n - 1) * doublefactorial;
+    }
+
+    return res;
+}
+
---
+++ ps2/3-1.cs
@@ -0,0 +1,76 @@
+﻿Console.WriteLine(Left(0, 2, 10000));
+Console.WriteLine(Right(0, 2, 10000));
+Console.WriteLine(Trapezium(0, 2, 10000));
+Console.WriteLine(MethodSimpson(0, 2, 1000000));
+Console.WriteLine(MethodMonteCarlo(0, 2, 5000));
+
+
+static double F(double x)
+{
+    return Math.Sin(x * x);
+}
+
+
+static double Left(double left, double right, double segmentCount)
+{
+    double segmentLength = 1 / segmentCount;
+    double res = 0;
+    for (double i = left; i < right; i += segmentLength)
+    {
+        res += F(i) * segmentLength;
+    }
+    return res;
+}
+
+static double Right(double left, double right, double segmentCount)
+{
+    double segmentLength = 1 / segmentCount;
+    double res = 0;
+    for (double i = left; i <= right - segmentLength; i += segmentLength)
+    {
+        res += F(i + segmentLength) * segmentLength;
+    }
+    return res;
+}
+
+static double Trapezium(double left, double right, double segmentCount)
+{
+    double segmentLength = 1 / segmentCount;
+    double res = 0;
+    for (double i = left; i <= right - segmentLength; i += segmentLength)
+    {
+        res += (F(i) + F(i + segmentLength)) / 2 * segmentLength;
+    }
+    return res;
+}
+
+static double MethodSimpson(double left, double right, double segmentCount)
+{
+    double res = 0;
+    double segmentLength = 0;
+    if (segmentCount % 2 == 1) segmentCount++;
+    segmentLength = 1 / segmentCount;
+    for (double i = left; i < right; i += segmentLength)
+    {
+        res += segmentLength / 6 * (F(i) + 4 * F((2 * i + segmentLength) / 2) + F(i + segmentLength));
+    }
+    return res;
+}
+
+
+static double MethodMonteCarlo(double left, double right, double segmentCount)
+{
+    double segmentXLength = 1 / segmentCount;
+    double segmentYLength = 2 / segmentCount;
+    double innerCount = 0, count = 0;
+    for (double x = left; x <= right; x += segmentXLength)
+    {
+        double f = F(x);
+        for (double y = 0; y <= 1; y += segmentYLength)
+        {
+            if (y >= 0 && y <= f) innerCount++;
+            count++;
+        }
+    }
+    return innerCount / count * 2;
+}
