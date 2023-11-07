f6ecacf4dfd3ebe32126d92115a613aba7ea73d7
ShabalinTK <108810276+ShabalinTK@users.noreply.github.com>
2023-11-07 23:10:43 +0300

    Add files via upload

---
+++ ДЗ 08.11/палиндром.cs
@@ -0,0 +1,21 @@
+﻿var str = "Казак";
+
+bool IsStringPalindrome(string str, int a, int b)
+{
+    foreach(var word in str)
+    {
+        var words = str.ToLower();
+        if (a >= b)
+        {
+            return true;
+        }
+
+        if (words[a] == words[b])
+        {
+            return IsStringPalindrome(str, a + 1, b - 1);
+        }
+    }
+
+    return false;
+}
+Console.WriteLine(IsStringPalindrome(str, 0, 4));
---
+++ ДЗ 08.11/симметрия.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = { 1, 2, 3, 3, 2, 1 };
+
+bool IsArraySymmetric(int[] arr, int a, int b)
+{
+    if (a >= b)
+    {
+        return true;
+    }
+
+    if (arr[a] == arr[b])
+    {
+        return IsArraySymmetric(arr, a + 1, b - 1);
+    }
+
+    return false;
+}
+Console.WriteLine(IsArraySymmetric(arr, 0, 5));
