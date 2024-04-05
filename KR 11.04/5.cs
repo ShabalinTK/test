c4f0a1d3633b8ab6e3ca5ed1c4e3b695db1d186c
Timofey <108810276+ShabalinTK@users.noreply.github.com>
2024-04-05 11:44:15 +0300

    Add files via upload

---
+++ KR 11.04/5.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int[] mas1 = { 1, 2, 3 };
+            int[] mas2 = { 4, 5, 6 };
+            int[] result = new int[mas1.Length + mas2.Length];
+
+            program.Copy(mas1, 0, result, 0, mas1.Length);
+            program.Copy(mas2, 0, result, mas1.Length, mas2.Length);
+
+            foreach (int e in result)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public T[] MergeArrays<T>(T[] mas1, T[] mas2)
+        {
+            T[] result = new T[mas1.Length + mas2.Length];
+            Copy(mas1, 0, result, 0, mas1.Length);
+            Copy(mas2, 0, result, mas1.Length, mas2.Length);
+            return result;
+        }
+
+        public T[] Copy<T>(T[] sourceArray, int sourceIndex, T[] destinationArray, int destinationIndex, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                destinationArray[destinationIndex + i] = sourceArray[sourceIndex + i];
+            }
+
+            return destinationArray;
+        }
+
+    }
+}
