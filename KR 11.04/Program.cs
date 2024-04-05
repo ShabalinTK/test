e6a53db0f46c5507fc833d4d99c63e6c48f4c0c6
Timofey <108810276+ShabalinTK@users.noreply.github.com>
2024-04-05 11:18:41 +0300

    Add files via upload

---
+++ KR 11.04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> str = new Dictionary<string, string>
+            {
+                { "Россия", "Москва" },
+                { "Германия", "Берлин" },
+                { "Франция", "Мариж" }
+            };
+
+            char capital = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            foreach (var e in str)
+            {
+                if (e.Value[0] == capital)
+                {
+                    Console.WriteLine(e.Key);
+                }
+            }
+        }
+    }
+}
